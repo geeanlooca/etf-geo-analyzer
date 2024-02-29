e3ac514bb1c78fedd796cf5d51302af36e96b903
Gianluca Marcon <marcon.gluca@gmail.com>
2024-03-01 00:02:11 +0100

    skeleton cli for providers

--- etfdiversipy/providers/__init__.py
+++ etfdiversipy/providers/__init__.py
@@ -10,23 +10,62 @@ class Provider(ABC):
 
 
 class Providers(Enum):
-    BLACKROCK = "blackrock"
     ISHARES = "ishares"
     LYXOR = "lyxor"
+    BLACKROCK = "blackrock"
     VANGUARD = "vanguard"
+
+
+class iShares(Provider):
+    def get_available_funds(self, *args, **kwargs):
+        raise NotImplementedError("iShares not implemented yet")
 
 
 class BlackRock(Provider):
     def get_available_funds(self, *args, **kwargs):
-        pass
+        raise NotImplementedError("BlackRock not implemented yet")
+
+
+class Lyxor(Provider):
+    def get_available_funds(self, *args, **kwargs):
+        raise NotImplementedError("Lyxor not implemented yet")
+
+
+class Vanguard(Provider):
+    def get_available_funds(self, *args, **kwargs):
+        raise NotImplementedError("Vanguard not implemented yet")
 
 
 def get_provider(provider: Providers) -> Provider:
-    if provider == "blackrock":
+    if provider == Providers.ISHARES:
+        return iShares()
+    if provider == Providers.BLACKROCK:
         return BlackRock()
-    else:
-        raise ValueError(f"Provider {provider} not supported")
+    if provider == Providers.LYXOR:
+        return Lyxor()
+    if provider == Providers.VANGUARD:
+        return Vanguard()
+
+    raise ValueError("Invalid provider")
 
 
 def get_providers() -> list[Provider]:
     ...
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Test the setup of a provider")
+    parser.add_argument("provider", help="Provider to use", type=Providers)
+    args = parser.parse_args()
+
+    try:
+        provider = get_provider(args.provider)
+        print(provider.get_available_funds())
+    except NotImplementedError as e:
+        print(e)
+        exit(1)
+    except ValueError as e:
+        print(e)
+        exit(1)
